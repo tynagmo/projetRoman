d6d4efa17e8db1ba6a44fb78da2d8bd2b6648ee3
tynagmo <tynagmo@gmail.com>
2017-06-22 07:32:20 -0400

    test : add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@NgModule({
+  declarations: [AppComponent],
+  imports: [NgbModule]
+})
+
 export class AppComponent {
-  title = 'app';
+  title = 'FloorPlan';
 }
